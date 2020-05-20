dc1e0f9b7e4f25977b4ae2019324f73a362ccd04
justin-wenning <wenning.justin@gmail.com>
2020-05-19 18:11:22 -0700

    Created sprite data class

--- main.js
+++ main.js
@@ -35,6 +35,27 @@ class App {
   }
 }
 
+// new SpriteData({x: 0, y: 0, w: 32, h: 32, f: 10}).render(this, 0, 10, 30, 30)
+class SpriteData {
+  constructor(...args) {
+    this.sprites = args;
+  }
+
+  render(app, index = 0, frame = 0, x = 0, y = 0) {
+    const { x: sx, y: sy, w, h, f } = this.sprites[index];
+    frame = frame % f;
+    app.ctx.drawImage(
+      app.sprites,
+      sx + w * frame,
+      sy, w, h,
+      app.unit * x,
+      app.unit * y,
+      app.unit * w,
+      app.unit * h);
+  }
+}
+
+
 const game = new App(document.querySelector('#gameWindow'));
 
 game.resize();
